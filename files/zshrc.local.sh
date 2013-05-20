2f525561479672fc546bdd292ddfb61b2ecac7c5
Jon Allured <jon.allured@gmail.com>
2013-05-20 15:10:08 -0500

    Remove sharpen from dotfiles

--- files/zshrc.local.sh
+++ files/zshrc.local.sh
@@ -1,12 +1,3 @@
-sharpen() {
-	local sharpener=~/Projects/dotfiles/sharpener.txt
-	if [ $@ ]; then
-		echo "- $@\n" >> $sharpener
-	else
-		cat $sharpener
-	fi
-}
-
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
