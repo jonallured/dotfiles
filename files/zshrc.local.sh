7fb2f47f312281eb6b5b065aeda65393e1e78fca
Jon Allured <jon.allured@gmail.com>
2013-05-20 16:11:49 -0500

    Add function to cd into code directory

--- files/zshrc.local.sh
+++ files/zshrc.local.sh
@@ -1,3 +1,9 @@
+c() {
+  cd "$HOME/code/$1"
+}
+
+compctl -/ -S '' -W "$HOME/code" c
+
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
