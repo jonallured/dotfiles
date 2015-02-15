a4499249065d811006a4e972fcccda5a7efc2818
Jon Allured <jon.allured@gmail.com>
2015-02-15 06:47:48 -0600

    Adjust some aliases

--- files/zshrc.local.sh
+++ files/zshrc.local.sh
@@ -8,16 +8,16 @@ alias ls='ls -aF'
 alias cheddar='git commit --amend -CHEAD'
 alias tree='git tree'
 alias trake='rspec -t ~live -fprogress && cucumber -t~@live -prake'
-alias leap=unicornleap
+alias leap='unicornleap'
 alias herd='unicornleap -n 3'
-alias s='jfa sharpen'
 alias dush='du -sh *'
 alias mm='middleman'
-alias gst='git status'
 alias k='heroku'
-alias kc='heroku run console'
+alias p='heroku run console -r production'
+alias s='heroku run console -r staging'
 alias deploy='script/deploy'
 alias hsync='script/hsync'
+alias solo='pear -u'
 
 eval "$(/Users/jon/code/hr/bin/hr init -)"
 eval "$(/Users/jon/code/jfa/bin/jfa init -)"
